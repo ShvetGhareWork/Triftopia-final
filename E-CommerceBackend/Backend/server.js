d5c2b21d3308d9ca51ab63b83811ab78996be204
ShvetGhare <ghareshvet@gmail.com>
2025-03-17 23:52:23 +0530

    CORS5 recognition

--- E-CommerceBackend/Backend/server.js
+++ E-CommerceBackend/Backend/server.js
@@ -25,6 +25,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   "https://triftopia-frontend.vercel.app",
+  "https://triftopia-admin.vercel.app",
 ];
 
 App.use(
